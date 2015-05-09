df8d81607eb7cee62763b40cded19552782908bd
Andreas Hellwig <a5308h@gmail.com>
2015-05-09 13:57:33 +0200

    This is actually necessary
    Found this out because I got lots of test data in develop

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,8 @@ require 'rspec/rails'
 require 'capybara/rspec'
 require 'capybara/rails'
 
+Rails.env = 'test'
+
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
