74e109af06e79b028f2fdf601c0c3f89d5e8b265
Andreas Hellwig <a5308h@gmail.com>
2015-05-09 13:53:15 +0200

    Adding courses and implementing first feature by showing the entered courses

--- spec/acceptance/viewing_the_root_page_spec.rb
+++ spec/acceptance/viewing_the_root_page_spec.rb
@@ -1,10 +1,11 @@
 require 'spec_helper'
 
-describe 'viewing the root page' do
+describe 'viewing the root page', type: :feature do
   let!(:only_course) { Course.create!(name: 'Dancing with the devils') }
 
   it 'shows the list of all courses' do
-    visit root_page
+    visit root_path
+
     expect(page).to have_content only_course.name
   end
 end
