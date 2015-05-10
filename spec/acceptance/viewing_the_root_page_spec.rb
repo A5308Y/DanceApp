6ce79d3e4167475b38e11640b0c9f3bc57649107
Andreas Hellwig <a5308h@gmail.com>
2015-05-10 10:19:06 +0200

    Allow login for a user

--- spec/acceptance/viewing_the_root_page_spec.rb
+++ spec/acceptance/viewing_the_root_page_spec.rb
@@ -3,9 +3,24 @@ require 'rails_helper'
 describe 'viewing the root page', type: :feature do
   let!(:only_course) { Course.create!(name: 'Dancing with the devils') }
 
-  it 'shows the list of all courses' do
+  before do
     visit root_path
+  end
 
+  it 'shows the list of all courses' do
     expect(page).to have_content only_course.name
+  end
+
+  context 'if a user exists' do
+    let!(:user) { User.create!(email: 'Maria.peter@example.com', password: 'password') }
+
+    it 'allows the user to log in' do
+      click_link 'Login'
+      fill_in 'Email', with: 'Maria.peter@example.com'
+      fill_in 'Password', with: 'password'
+      click_button 'Log in'
+
+      expect(page).to have_content 'Logged in as maria.peter@example.com'
+    end
   end
 end
