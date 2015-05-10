1db6b11db61a5c9e278a9677bcf2545d854c7069
Andreas Hellwig <a5308h@gmail.com>
2015-05-10 20:29:12 +0200

    Adjusting for README

--- spec/acceptance/creating_a_course_spec.rb
+++ spec/acceptance/creating_a_course_spec.rb
@@ -1,18 +1,16 @@
 require 'rails_helper'
 
-describe 'registering a new course', type: :feature do
+describe 'Registering a new course', type: :feature do
   context "When I'm logged in as a teacher" do
     let!(:teacher) { log_in_as :teacher }
 
-    describe 'registering a new course' do
-      it 'allows me to enter its location' do
-        click_link 'Neuer Kurs'
-        fill_in 'Name', with: 'Flying below'
-        fill_in 'Ort', with: 'Tanzfabrik Berlin'
-        click_button 'Kurs registrieren'
+    it 'allows me to enter its location' do
+      click_link 'Neuer Kurs'
+      fill_in 'Name', with: 'Flying below'
+      fill_in 'Ort', with: 'Tanzfabrik Berlin'
+      click_button 'Kurs registrieren'
 
-        expect(page).to have_content 'Flying below'
-      end
+      expect(page).to have_content 'Flying below'
     end
   end
 end
