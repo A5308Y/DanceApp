435e204f5e51b8cc25496f8a82d80ad1df807553
Andreas Hellwig <a5308h@gmail.com>
2015-05-13 20:38:51 +0200

    Moving spec to features using login helper

--- spec/features/creating_a_course_spec.rb
+++ spec/features/creating_a_course_spec.rb
@@ -2,10 +2,12 @@ require 'rails_helper'
 
 describe 'Registering a new course', type: :feature do
   context "When I'm logged in as a teacher" do
-    let!(:teacher) { log_in_as :teacher }
+    let!(:teacher) { User.create!(email: 'user@example.com', password: '123secret') }
 
     it 'allows me to enter its location' do
+      log_in_as teacher
       click_link 'Neuer Kurs'
+
       fill_in 'Name', with: 'Flying below'
       fill_in 'Ort', with: 'Tanzfabrik Berlin'
       click_button 'Kurs registrieren'
